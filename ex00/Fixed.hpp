9407a29e3c1ac8fa0eda1b5b1bd2ce3356ed0f6e
SabaLeonel <leonel.saba@hotmail.com>
2023-11-22 18:39:14 +0100

    ex00 done

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,20 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+    public:
+        Fixed(void);
+        Fixed(const Fixed &other);
+        ~Fixed(void);
+        Fixed &operator=(const Fixed &other);
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+    private:
+        int _fixedPointValue;
+        static const int _fractionalBits = 8;
+};
+
+#endif
---
+++ ex01/Fixed.cpp
@@ -0,0 +1,66 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void) : _fixedPointValue(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed::Fixed(const int value)
+{
+    std::cout << "Int constructor called" << std::endl;
+    this->_fixedPointValue = value << this->_fractionalBits;
+}
+
+Fixed::Fixed(const float value)
+{
+    std::cout << "Float constructor called" << std::endl;
+    this->_fixedPointValue = roundf(value * (1 << this->_fractionalBits));
+}
+
+Fixed::~Fixed(void)
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+float Fixed::toFloat(void) const
+{
+    return (float)this->_fixedPointValue / (1 << this->_fractionalBits);
+}
+
+int Fixed::toInt(void) const
+{
+    return this->_fixedPointValue >> this->_fractionalBits;
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed)
+{
+    out << fixed.toFloat();
+    return out;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other)
+        this->_fixedPointValue = other.getRawBits();
+    return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_fixedPointValue;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    this->_fixedPointValue = raw;
+}
+
